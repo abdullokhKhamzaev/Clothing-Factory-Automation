11c527c1ffa920e9b2bc12b34d386687b91c2329
Abdullokh <abdullokh.khamzaev@gmail.com>
2024-12-11 12:17:07 +0500

    Setting up axios

--- src/stores/authorization.js
+++ src/stores/authorization.js
@@ -1,9 +1,9 @@
 import { defineStore } from "pinia";
-import axios from "axios";
+import { client } from "src/boot/axios.js";
 export const useAuthorization = defineStore('authorization', () => {
     function userAuth(data) {
         return new Promise((resolve, reject) => {
-            axios.post('http://localhost:8888/api/users/auth', data)
+          client.post('users/auth', data)
                 .then((res) => {
                     console.log('Token got');
                     localStorage.setItem('token', res.data.token);
