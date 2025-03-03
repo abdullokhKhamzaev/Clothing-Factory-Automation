53e869063925c5185531790f914fd4cfec10383d
Abdullokh Khamzaev <abdullokh.khamzaev@gmail.com>
2025-03-03 12:32:46 +0500

    Enabling the feature of searching on selectableList component

--- src/components/selectableList.vue
+++ src/components/selectableList.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { computed, onMounted, ref } from "vue";
 
+const stringOptions = ref([]);
+
 const props = defineProps({
   store: {
     type: Object,
@@ -61,6 +63,7 @@ const props = defineProps({
 const selectedItem = ref(null);
 const total = ref(10);
 const items = ref([]);
+const filteredOptions = ref([]);
 const loading = ref(false);
 const page = ref(1);
 
@@ -85,6 +88,8 @@ async function getItems(filterProps = {}) {
     const response = await props.store[props.fetchMethod](filterProps);
     total.value = response.data[props.totalItemsKey];
     items.value = [...items.value, ...response.data[props.itemsKey]];
+    stringOptions.value = options.value;
+    filteredOptions.value = options.value;
     page.value++;
   } catch (e) {
     console.error(e);
@@ -99,14 +104,30 @@ const loadMore = () => {
   }
 };
 
+function filterFn(val, update) {
+  if (val === '') {
+    update(() => {
+      filteredOptions.value = stringOptions.value;  // Reset to original options
+    });
+    return;
+  }
+
+  update(() => {
+    const needle = val.toLowerCase();
+    filteredOptions.value = stringOptions.value.filter(v => v.label.toLowerCase().indexOf(needle) > -1);  // Filter the options
+  });
+}
+
 onMounted(() => {
   getItems({ page: page.value });
-})
+});
 </script>
 
 <template>
   <q-select
     v-model="selectedItem"
+    use-input
+    input-debounce="0"
     :disable="disable"
     :multiple="multiple"
     :clearable="clearable"
@@ -114,12 +135,13 @@ onMounted(() => {
     emit-value
     map-options
     :loading="loading"
-    :options="options"
+    :options="filteredOptions"
     :label="label"
     option-value="value"
     option-label="label"
     :rules="[val => !!val || ruleMessage]"
     hide-bottom-space
+    @filter="filterFn"
     @virtual-scroll="loadMore"
   >
     <template v-slot:loading>
