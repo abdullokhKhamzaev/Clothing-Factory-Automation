18f4e156d003156c9f39d965830a1cda32373cd3
Abdullokh Khamzaev <abdullokh.khamzaev@gmail.com>
2026-03-04 15:08:27 +0500

    Giving permission for test

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { useRefreshToken } from 'src/stores/user/refreshToken.js'
 import { useAbout } from "stores/user/about.js";
 import { onMounted, ref } from "vue";
+const user = useAbout();
 
 const accessToken = ref(localStorage.getItem('accessToken'));
 
@@ -20,14 +21,15 @@ onMounted(() => {
 })
 </script>
 <template>
-  <div id="q-app" class="flex full-height justify-center items-center" style="min-height: 100vh">
-      <div>
-        <h1 class="text-h5 text-primary"> Revizya jarayonida: </h1>
-        <q-linear-progress dark stripe rounded size="20px" :value="0.8" color="orange" />
-      </div>
+  <div id="q-app" v-if="user?.about?.phone == '+998917070708'">
+    <router-view />
   </div>
-<!--  <div id="q-app">-->
-<!--    <router-view />-->
-<!--  </div>-->
+
+  <div v-else id="q-app" class="flex full-height justify-center items-center" style="min-height: 100vh">
+    <div>
+      <h1 class="text-h5 text-primary"> Revizya jarayonida: </h1>
+      <q-linear-progress dark stripe rounded size="20px" :value="0.8" color="orange" />
+    </div>
+  </div>
 </template>
 
