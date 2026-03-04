9649f2f98038376cd95e9d8933758b9818184568
Abdullokh Khamzaev <abdullokh.khamzaev@gmail.com>
2026-03-04 15:35:37 +0500

    putting back

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { useRefreshToken } from 'src/stores/user/refreshToken.js'
 import { useAbout } from "stores/user/about.js";
 import { onMounted, ref } from "vue";
-const user = useAbout();
 
 const accessToken = ref(localStorage.getItem('accessToken'));
 
@@ -21,11 +20,7 @@ onMounted(() => {
 })
 </script>
 <template>
-  <div id="q-app" v-if="user?.about?.phone == '+998917070708'">
-    <router-view />
-  </div>
-
-  <div v-else id="q-app" class="flex full-height justify-center items-center" style="min-height: 100vh">
+  <div id="q-app" class="flex full-height justify-center items-center" style="min-height: 100vh">
     <div>
       <h1 class="text-h5 text-primary"> Revizya jarayonida: </h1>
       <q-linear-progress dark stripe rounded size="20px" :value="0.8" color="orange" />
